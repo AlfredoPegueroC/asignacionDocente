7d3d8957174f9319ebe64cda38945a72acb5e420
AlfredoPegueroC <alfredocapellan20@gmail.com>
2025-02-16 16:37:30 -0400

    static update

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,10 @@
 
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     #URLS CREATE
     path('', views.index, name='index'),

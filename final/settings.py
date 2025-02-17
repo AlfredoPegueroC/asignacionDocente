0cd1aa74dc89ce5225eca51a25dbad6ec60d813e
AlfredoPegueroC <alfredocapellan20@gmail.com>
2025-02-17 13:02:08 -0400

    DATABASE changes

--- final/settings.py
+++ final/settings.py
@@ -92,17 +92,22 @@ WSGI_APPLICATION = 'final.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'ivqNPfyEZwPdqBGEziGEAYLvbDSAoHzJ',
+#         'HOST': 'maglev.proxy.rlwy.net',
+#         'PORT': '34104',  
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'ivqNPfyEZwPdqBGEziGEAYLvbDSAoHzJ',
-        'HOST': 'maglev.proxy.rlwy.net',
-        'PORT': '34104',  
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators

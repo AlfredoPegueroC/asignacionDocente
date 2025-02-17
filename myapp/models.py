8967be3f8ef1726545ee6dcaffd4a2e0e510e746
AlfredoPegueroC <alfredocapellan20@gmail.com>
2025-02-16 23:54:36 -0400

    migrations folder back

--- myapp/models.py
+++ myapp/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Universidad(models.Model):
     UniversidadCodigo = models.AutoField(primary_key=True,editable=False)
-    nombre = models.CharField(max_length=100, null=False)
+    nombre = models.CharField(max_length=110, null=False)
     estado = models.CharField(
         max_length=15,
         choices=[('Activo', 'Activo'), ('Inactivo', 'Inactivo')]
